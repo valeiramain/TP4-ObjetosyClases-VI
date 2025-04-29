86ad0060fe3564beeffef9c976cac59798e90ab0
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-29 16:53:22 -0300

    queda pendiente si se coloca condicion logica para repetir las tareas de prendido y apagado

--- js/tp4-ejercicio1.js
+++ js/tp4-ejercicio1.js
@@ -8,27 +8,51 @@
 // El auto se apagó
 
 const auto = {
-    color : 'Gris',
-    marca : 'Toyota',
+    color: 'Gris',
+    marca: 'Toyota',
     modelo: 'Hylux',
-    encendido : true,
+    encendido: false,
 
-    encender: function(){
-        this.encendido=true;
-        document.writeln("Auto encendido")
+    encender: function () {
+        this.encendido = true;
+        document.writeln(`<p>Auto encendido</p>`)
     },
 
-    apagar: function(){
-        this.encendido=false;
-        document.writeln("El auto se apagó")
+    apagar: function () {
+        this.encendido = false;
+        document.writeln(`<p>El auto se apagó</p>`)
     },
 }
 
-if (!auto.encendido){
-        encender()
-}else{
-    apagar()
-}
+console.log(auto)
+console.log(auto.encendido)
 
+const accion = parseInt(prompt("Ingrese 1: Prender Auto, 2: Apagar Auto. "))
+console.log('accion: ' + accion)
+
+switch (accion) {
+    case 1:
+        console.log('case 1'+auto.encendido)
+        if (auto.encendido === false) {
+            console.log('prender')
+            auto.encender()
+            break;
+        } else {
+            document.writeln("<p>El auto ya está encendido</p>")
+        }
+
+    case 2:
+        console.log('case 2'+auto.encendido)
+        if (auto.encendido === true) {
+            console.log('apagar')
+            auto.apagar()
+        } else {
+            document.writeln("<p>El auto ya está apagado</p>")
+        }
+        break;
+    default:
+        document.writeln(`<p>Ingresaste una opción inválida</p>`)
+        break;
+}
 
 
