ac43d8dd9cad9378345429f7faab8b15a86a73a6
Valentina Iramain <valentinairamain2020@gmail.com>
2025-05-01 16:37:48 -0300

    Se realiza ejericio 3, pero hay dudas de cuando usar getter

---
+++ js/tp4-ejercicio3.js
@@ -0,0 +1,72 @@
+//3-Escribe una clase que permita crear distintos objetos “rectángulos”,
+// con las propiedades de alto y ancho, mas los métodos necesarios para modificar
+// y mostrar sus propiedades, calcular el perímetro y el área
+
+
+class Rectangulos {
+    // Defino cada propiedad privada
+    #alto;
+    #ancho;
+
+    constructor(altop, anchop) {
+        this.#alto = altop;
+        this.#ancho = anchop;
+    }
+
+    // propiedades computadas: se usan cuando tengo propiedades privadas
+    //get = leer un valor
+    get getAlto() {
+        return this.#alto
+    }
+    get getAncho() {
+        return this.#ancho
+    }
+
+    //set = modificar valor. siempre tiene parametros
+    set setAlto(nuevoAlto) {
+        this.#alto = nuevoAlto
+    }
+    set setAncho(nuevoAncho) {
+        this.#ancho = nuevoAncho
+    }
+
+    //metodos
+
+    perimetro() {
+        return (2 * (this.#alto + this.#ancho))
+    }
+
+    area() {
+        return (this.#alto * this.#ancho)
+    }
+
+    mostrarDatos(titulo) {
+        document.writeln(`<h3>${titulo}</h3>
+            <ul>
+            <li>Alto: ${this.#alto}</li>
+            <li>Ancho: ${this.#ancho}</li>
+            </ul>`)
+    }
+
+}
+
+
+const rectangulo1 = new Rectangulos(2, 10);
+rectangulo1.mostrarDatos(`Nuevo Rectángulo`)
+
+document.writeln(`<p>Perímetro = ${rectangulo1.perimetro()}</p>`)
+document.writeln(`<p>Area = ${rectangulo1.area()}</p>`)
+
+// modificar propiedades
+rectangulo1.setAlto = 5;
+rectangulo1.setAncho = 20;
+rectangulo1.mostrarDatos(`Rectángulo Modificado`);
+
+document.writeln(`<p>Perímetro = ${rectangulo1.perimetro()}</p>`)
+document.writeln(`<p>Area = ${rectangulo1.area()}</p>`)
+
+
+
+
+
+
