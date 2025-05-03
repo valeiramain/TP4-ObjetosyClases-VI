bb12660f22be2ce68eb1375825009142fc246b17
Valentina Iramain <valentinairamain2020@gmail.com>
2025-05-03 16:40:57 -0300

    se crea switch para cada opcion a realizar en la agenda

--- js/tp4-ejercicio7.js
+++ js/tp4-ejercicio7.js
@@ -26,22 +26,85 @@ class Contacto {
         this.#nombre = nombrep;
         this.#telefono = telefonop;
     }
+
+    // propiedades computadas: se usan cuando tengo propiedades privadas
+    //get = leer un valor
+    get getNombre() {
+        return this.#nombre
+    }
+    get getTelefono() {
+        return this.#telefono
+    }
+
+    //set = modificar valor. siempre tiene parametros
+    set setNombre(nuevoNombre) {
+        this.#nombre = nuevoNombre
+    }
+    set setTelefono(nuevoTelefono) {
+        this.#telefono = nuevoTelefono
+    }
+
+    // agregarContacto(arrayAgenda) {
+    //     const cantContactos = parseInt(prompt('Ingrese cantidad de contactos a Ingresar en la Agenda: '))
+    // if (!isNaN(cantContactos) && (cantContactos <= 10)) {
+    //     for (let i = 0; i < cantContactos; i++) {
+    //         const nuevoNombre = prompt('Ingrese Nombre:')
+    //         const nuevoTelefono = prompt('Ingrese Telefono:')
+    //         const nuevoContacto = new Contacto(nuevoNombre, nuevoTelefono)
+    //         console.log(nuevoContacto)
+    //         arrayAgenda.push(nuevoContacto)
+    //         console.log(arrayAgenda)
+    //     }
+    // }
+    // } else {
+    //     document.writeln(`<p>Operación Inválida</p>`)
+    // }
+
 }
 
 
-const cantContactos = parseInt(prompt('Ingrese cantidad de contactos a Ingresar en la Agenda: '))
 
-if (!isNaN(cantContactos) && (cantContactos<=10)) {
-    for (let i = 0; i < cantContactos; i++) {
-        const nuevoNombre = prompt('Ingrese Nombre:')
-        const nuevoTelefono = prompt('Ingrese Telefono:')
 
-        const nuevoContacto = new Contacto(nuevoNombre,nuevoTelefono)
-        console.log(nuevoContacto)
-        arrayAgenda.push(nuevoContacto)
-        console.log(arrayAgenda)
+const operacion = parseInt(prompt('Ingrese 1=>Agregar 2=>Existe 3=>Listar 4=>Buscar 5=>Eliminar 6=>Agenda Llena 7=>Huecos Libres: '))
+
+
+if (!isNaN(operacion) && (operacion >= 1 || operacion <= 7)) {
+    switch (operacion) {
+        case 1:
+            //Agregar
+            console.log('Agregar')
+            break;
+        case 2:
+            //Existe?
+            console.log('Existe?')
+            break;
+        case 3:
+            //Listar
+            console.log('Listar')
+            break;
+        case 4:
+            //Buscar
+            console.log('Buscar')
+            break;
+        case 5:
+            //Eliminar
+            console.log('eliminar')
+            break;
+        case 6:
+            //Agenda llena?
+            console.log('llena?')
+            break;
+        case 7:
+            //huecos libres?
+            console.log('huecos?')
+            break;
+        default:
+            document.writeln("Ingresaste una opción inválida")
     }
 } else {
     document.writeln(`<p>Operación Inválida</p>`)
 }
+
+
+
 console.log('fin programa')
